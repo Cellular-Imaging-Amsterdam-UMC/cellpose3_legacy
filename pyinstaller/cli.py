82e47e4f5a38bfc6403ddec68fbeee0a767231c5
Ron Hoebe <46439648+RAHoebe@users.noreply.github.com>
2026-05-05 09:42:16 +0200

    trimmed repo

--- pyinstaller/cli.py
+++
@@ -1,4 +0,0 @@
-from cellpose3_legacy.__main__ import main
-
-if __name__ == '__main__':
-    main()
--- setup.py
+++ setup.py
@@ -17,34 +17,6 @@ install_deps = [
     'fill-voids',
 ]
 
-image_deps = ['nd2', 'pynrrd']
-
-gui_deps = [
-    'pyqtgraph>=0.11.0rc0', "pyqt6", "pyqt6.sip", 'qtpy', 'superqt',
-]
-
-docs_deps = [
-    'sphinx>=3.0',
-    'sphinxcontrib-apidoc',
-    'sphinx_rtd_theme',
-    'sphinx-argparse',
-]
-
-distributed_deps = [
-    'dask',
-    'distributed',
-    'dask_image',
-    'pyyaml',
-    'zarr',
-    'dask_jobqueue',
-    'bokeh',
-    'pyarrow',
-]
-
-bioimageio_deps = [
-    'bioimageio.core',
-]
-
 try:
     import torch
     a = torch.ones(2, 3)
@@ -56,49 +28,27 @@ try:
 except:
     pass
 
-try:
-    import PyQt5
-    gui_deps.remove("pyqt6")
-    gui_deps.remove("pyqt6.sip")
-    gui_deps.append("pyqt5")
-    gui_deps.append("pyqt5.sip")
-except:
-    pass
-
-try:
-    import PySide2
-    gui_deps.remove("pyqt6")
-    gui_deps.remove("pyqt6.sip")
-except:
-    pass
-
-try:
-    import PySide6
-    gui_deps.remove("pyqt6")
-    gui_deps.remove("pyqt6.sip")
-except:
-    pass
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="cellpose3_legacy", license="BSD", author="Marius Pachitariu and Carsen Stringer",
+    name="cellpose3_legacy",
+    license="BSD",
+    author="Marius Pachitariu and Carsen Stringer",
     author_email="stringerc@janelia.hhmi.org",
-    description="Cellpose 3 legacy fork (cellpose <4) for coexistence with cellpose >=4", long_description=long_description,
+    description="Cellpose 3 legacy fork (cellpose <4) for coexistence with cellpose >=4",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Cellular-Imaging-Amsterdam-UMC/cellpose3_legacy", setup_requires=[
-        'pytest-runner',
-        'setuptools_scm',
-    ], packages=setuptools.find_packages(), use_scm_version=True,
-    install_requires=install_deps, tests_require=['pytest'], extras_require={
-        'docs': docs_deps,
-        'gui': gui_deps,
-        'distributed': distributed_deps,
-        'bioimageio': bioimageio_deps,
-        'all': gui_deps + distributed_deps + image_deps + bioimageio_deps,
-    }, include_package_data=True, classifiers=(
+    url="https://github.com/Cellular-Imaging-Amsterdam-UMC/cellpose3_legacy",
+    setup_requires=['setuptools_scm'],
+    packages=setuptools.find_packages(),
+    use_scm_version=True,
+    install_requires=install_deps,
+    include_package_data=True,
+    classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-    ), entry_points={'console_scripts': ['cellpose3_legacy = cellpose3_legacy.__main__:main']})
+    ),
+    entry_points={'console_scripts': ['cellpose3_legacy = cellpose3_legacy.__main__:main']},
+)
